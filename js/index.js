07651133c175acd40ee7f3dfee420a2b3125b1a6
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-05-15 11:03:57 -0700

    Added description from json and styled for local env

--- js/index.js
+++ js/index.js
@@ -1,33 +1,49 @@
-/*global d3*/
-function buildChart(ds) {
-    /* ds is a nested array containing text date and numeric value
-    ["2015-04-01", 17913.7], ... */
-    var w = 700;
-    var h = 400;
+/* global d3 */
+var w = 700;
+var h = 400;
 
-    var yScale = d3.scale.linear()
-                  .domain([0, d3.max(ds, function(d) {return d[1];})])
-                  .range([0, h])
-
-    var svg = d3.select('#root').append('svg').attr({width: w, height: h, class: 'chart'});
-    svg.selectAll('rect')
-        .data(ds)
-        .enter()
-        .append('rect')
-            .attr('x', function(d,i) {
-                return (i * w / ds.length);
-            })
-            .attr('y', function(d) {
-                return h - yScale(d[1]);
-            })
-            .attr('width', w / ds.length)
-            .attr('height', function(d){
-              return yScale(d[1]);
-            })
+function addDescription(desc) {
+  d3.select('#root').append('div')
+    .attr('class', 'description')
+    .style('width', w + 'px')
+    .html(desc);
 }
-d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function(error, data) {
-  if(error) {
+function buildChart(ds) {
+  /* ds is a nested array containing text date and numeric value
+  ["2015-04-01", 17913.7], ... */
+
+  var yScale = d3.scale.linear()
+    .domain([0, d3.max(ds, function(d) {
+      return d[1];
+    })])
+    .range([0, h]);
+
+  var svg = d3.select('#root').append('svg').attr({
+    width: w,
+    height: h,
+    class: 'chart'
+  });
+  svg.selectAll('rect')
+    .data(ds)
+    .enter()
+    .append('rect')
+    .attr('x', function(d, i) {
+      return (i * w / ds.length);
+    })
+    .attr('y', function(d) {
+      return h - yScale(d[1]);
+    })
+    .attr('width', w / ds.length)
+    .attr('height', function(d) {
+      return yScale(d[1]);
+    });
+}
+d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json',
+function(error, data) {
+  if (error) {
     console.log(error);
   }
+  // console.log(data);
   buildChart(data.data);
+  addDescription(data.description);
 });
